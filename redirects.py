0d5d65a5ebd25b70117b6b48ec1e734e07dff452
Chris Mayfield <mayfiecs@jmu.edu>
2026-01-31 11:05:14 -0500

    Change redirects.py to use a time window instead of base commit

--- redirects.py
+++ redirects.py
@@ -1,70 +1,65 @@
-"""Generate apache redirect rules by following page history in git log."""
+"""Generate apache redirect rules by following page history in the git log."""
 
 import subprocess
 from pathlib import Path
 
-# Last change on DokuWiki before the migration
-BASE_COMMIT = "45ced82"
+# Redirect window
+SINCE = "6 months ago"
 
-# Path to Markdown source files on the site
+# Path to Markdown source files
 DOCS_ROOT = "docs"
 
 
 def run(cmd: list[str]) -> list[str]:
-    """Run command with arguments and return its output."""
-    return subprocess.check_output(cmd, text=True, stderr=subprocess.DEVNULL).splitlines()
+    """Run a command with arguments and return its output."""
+    return subprocess.check_output(cmd, text=True).splitlines()
 
 
 def current_pages() -> list[Path]:
-    """Get relative paths to all Markdown pages in docs."""
+    """Get relative paths to all current Markdown pages."""
     return [Path(p) for p in run(["git", "ls-files", DOCS_ROOT]) if p.endswith(".md")]
 
 
-def path_at_base_commit(path: Path) -> Path | None:
-    """Get relative path of page at BASE_COMMIT if existed."""
-
-    # Get the old version of the path
+def previous_paths(path: Path) -> list[Path]:
+    """Get all previous paths to the given Markdown page."""
     output = run([
-        "git", "log", f"{BASE_COMMIT}..HEAD",
+        "git", "log", f"--since={SINCE}",
         "--follow", "--name-status", "--format=",
         "--", str(path)
     ])
-
-    # Path on last line, second word
-    old = output[-1].split()[1]
-
-    # Verify the path actually existed
-    try:
-        run(["git", "cat-file", "-e", f"{BASE_COMMIT}:{old}"])
-    except subprocess.CalledProcessError:
-        return None
-
-    return DOCS_ROOT / Path(old)
+    # Get the second word (old path) of each line with rename status
+    return [Path(line.split()[1]) for line in output if line.startswith("R")]
 
 
 def path_to_url(path: Path, index_name: str) -> str:
     """Convert a source file path to a normalized url."""
-    path = path.relative_to(DOCS_ROOT)
+    # Remove docs root if present
+    if path.is_relative_to(DOCS_ROOT):
+        path = path.relative_to(DOCS_ROOT)
+    # Remove index file if present
     if path.name == index_name:
         return str(path.parent)
+    # Remove extension (txt or md)
     return str(path.with_suffix(""))
 
 
 def main():
+    rules: set[tuple[str, str]] = set()
+    current_urls = {path_to_url(p, "index.md") for p in current_pages()}
+
     for new_path in current_pages():
-        old_path = path_at_base_commit(new_path)
-        if not old_path:
-            continue
+        for old_path in previous_paths(new_path):
+            # Convert the source file paths to urls
+            old_url = path_to_url(old_path, "start.txt")
+            new_url = path_to_url(new_path, "index.md")
+            # Emit rule only if the path has changed and will not shadow an existing page
+            if old_url != new_url and old_url not in current_urls:
+                rules.add((old_url, new_url))
 
-        # Convert the source file paths to urls
-        old_url = path_to_url(old_path, "start.txt")
-        new_url = path_to_url(new_path, "index.md")
-
-        # Emit rule only if the path has changed
-        if old_url != new_url:
-            # The old URL should not end with a slash, because Apache matches prefixes.
-            # The new URL should end with a slash, because it represents a directory.
-            print(f"Redirect 301 /{old_url} /{new_url}/")
+    for old_url, new_url in sorted(rules, key=lambda r: r[1]):
+        # The old URL should not end with a slash, because Apache matches prefixes.
+        # The new URL should end with a slash, because it represents a directory.
+        print(f"Redirect 301 /{old_url} /{new_url}/")
 
 
 if __name__ == "__main__":
